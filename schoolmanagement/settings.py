ab0b569ea81ddb0428968f98b60462b81db788b2
uttaran-cut <62888148+uttaran-cut@users.noreply.github.com>
2025-11-03 12:26:35 +0530

    Update settings.py

--- schoolmanagement/settings.py
+++ schoolmanagement/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'aw=%fl(=n$r@bci1ct$^wxnc-w#i_lr$jx_b9%(^6&j%^5l-v1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
